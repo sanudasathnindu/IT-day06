b7772adcb2a1575e1f67f4e47e120f97e3bc06d1
sanudasathnindu <sanudasathnindu5@gmail.com>
2026-07-08 22:12:54 +0530

    commit02

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+class Customer{
+    name;
+    age;
+    address;
+
+    constructor(name, age, address){
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+}
+
+let customer1 = new Customer("John Doe", 30, "123 Main St");
+console.log(customer1); 
