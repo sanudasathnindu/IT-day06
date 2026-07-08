f58c874d6c9dd007d398d82c6cfa660ef2e9a6e8
sanudasathnindu <sanudasathnindu5@gmail.com>
2026-07-08 22:30:07 +0530

    commit04

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-class Customer{
-    name;
-    age;
-    address;
+// class Customer{
+//     name;
+//     age;
+//     address;
 
-    constructor(name, age, address){
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
+//     constructor(name, age, address){
+//         this.name = name;
+//         this.age = age;
+//         this.address = address;
+//     }
 
-}
+// }
 
-let customer1 = new Customer("John Doe", 30, "123 Main St");
-console.log(customer1); 
+// let customer1 = new Customer("John Doe", 30, "123 Main St");
+// console.log(customer1); 
 
 let customer2 ={
     name: "Jane Smith",
     age: 25,
     address: "456 Elm St"
 }
-console.log(customer2);
+console.log(customer2.name);
+console.log(customer2.age);
+console.log(customer2.address);
+
