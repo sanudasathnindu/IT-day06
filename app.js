2e9ae858046eab786bcf796ed4068c6239edaab7
sanudasathnindu <sanudasathnindu5@gmail.com>
2026-07-23 22:46:05 +0530

    commit08

--- app.js
+++ app.js
@@ -14,57 +14,65 @@
 // let customer1 = new Customer("John Doe", 30, "123 Main St");
 // console.log(customer1); 
 
-let customer2 ={
-    name: "Jane Smith",
-    age: 25,
-    address: "456 Elm St",
-    salary: 50000,
-    items: [
-        {
-            id: 1,
-            name: "item1",
-            price: 100,
-            variants: [
-                {
-                    id: 1,
-                    name: "variant1",
-                    price: 150
-                },{
-                    id: 2,
-                    name: "variant2",
-                    price: 200
-                },{
-                    id: 3,
-                    name: "variant3",
-                    price: 250
-                }
-            ]
-        },{
-            id: 2,
-            name: "item2",
-            price: 200
-        },{
-            id: 3,
-            name: "item3",
-            price: 300
-        }
-    ],
-    father: {
-        name: "Robert Smith",
-        age: 55,
-        address: "789 Oak St"
-    }
-}
-console.log(customer2.name);
-console.log(customer2.age);
-console.log(customer2.address);
-console.log(customer2.salary);
-console.log(customer2.items);
-console.log(customer2.father);
-console.log(customer2.items[0].name);
-console.log(customer2.items[0].variants[1].name);
-console.log(customer2.items[0].variants[1].id);
-console.log(customer2.items[0].variants[1].price);
-console.log(customer2.items[0].variants[2].name);
-console.log(customer2.items[0].variants[2].id);
-console.log(customer2.items[0].variants[2].price);
+// let customer2 ={
+//     name: "Jane Smith",
+//     age: 25,
+//     address: "456 Elm St",
+//     salary: 50000,
+//     items: [
+//         {
+//             id: 1,
+//             name: "item1",
+//             price: 100,
+//             variants: [
+//                 {
+//                     id: 1,
+//                     name: "variant1",
+//                     price: 150
+//                 },{
+//                     id: 2,
+//                     name: "variant2",
+//                     price: 200
+//                 },{
+//                     id: 3,
+//                     name: "variant3",
+//                     price: 250
+//                 }
+//             ]
+//         },{
+//             id: 2,
+//             name: "item2",
+//             price: 200
+//         },{
+//             id: 3,
+//             name: "item3",
+//             price: 300
+//         }
+//     ],
+//     father: {
+//         name: "Robert Smith",
+//         age: 55,
+//         address: "789 Oak St"
+//     }
+// }
+// console.log(customer2.name);
+// console.log(customer2.age);
+// console.log(customer2.address);
+// console.log(customer2.salary);
+// console.log(customer2.items);
+// console.log(customer2.father);
+// console.log(customer2.items[0].name);
+// console.log(customer2.items[0].variants[1].name);
+// console.log(customer2.items[0].variants[1].id);
+// console.log(customer2.items[0].variants[1].price);
+// console.log(customer2.items[0].variants[2].name);
+// console.log(customer2.items[0].variants[2].id);
+// console.log(customer2.items[0].variants[2].price);
+
+
+
+// DOM Manipulation
+
+document.write("<h2>DOM Manipulation</h2>");
+
+console.log(document.title);
